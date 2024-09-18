ff92d5132dd4cc40a6eddbff16b8aa26231a10c6
ernest <shiwb@shideMacBook-Pro.local>
2024-09-18 16:13:48 +0800

    hunter: update the supply.ts

--- potv/scripts/supply.ts
+++ potv/scripts/supply.ts
@@ -17,12 +17,16 @@ async function main() {
     await tx.wait();
     console.log("Supply successful", tx.hash);
 
+
+    const validator1 = config.validators[0];
+    const lend = await ethers.getContractAt("Lend", ContractAdds.Lend);
+    const tx = await lend.supply(ContractAdds.Collateral_1, ethers.parseEther('10'), validator1);
+    await tx.wait();
+    console.log("Supply successful", tx.hash);
+
     const tx1 = await lend.withdraw(ContractAdds.Collateral_1, ethers.parseEther('1'), validator1);
     await tx1.wait();
-    console.log("Supply successful", tx1.hash);
-
-
-
+    console.log("withdraw successful", tx1.hash);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
